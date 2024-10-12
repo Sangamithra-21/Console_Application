fc34e2f96d8a175e727dbcb1daf037f8ed89d70b
Sangamithra-21 <117377739+Sangamithra-21@users.noreply.github.com>
2024-10-12 20:54:05 +0530

    Add files via upload

--- EmailApplication/FileHandler.java
+++ EmailApplication/FileHandler.java
@@ -39,10 +39,6 @@ public class FileHandler {
 
         String currLine = "";
 
-        String regexPattern = convertToRegex(searchWord);
-
-
-        Pattern pattern = Pattern.compile(regexPattern);
 
         boolean findValue = false;
 
@@ -53,9 +49,7 @@ public class FileHandler {
 
             for (int i = 1; i < mails.length; i++) {
 
-                    Matcher matcher = pattern.matcher(mails[i]);
-
-                    if (matcher.find() && !set.contains(currLine)) {
+                    if ((currLine.contains(searchWord) || isMatch(mails[i],searchWord)) && !set.contains(currLine)) {
                         System.out.println(currLine);
                         set.add(currLine);
                         findValue = true;
@@ -72,14 +66,44 @@ public class FileHandler {
         reader.close();
     }
 
+    private boolean isMatch(String s,String p)
+    {
+        int n = s.length();
+        int m = p.length();
 
-    private String convertToRegex(String searchWord) {
+        boolean[][] dp = new boolean[n+1][m+1];
 
-        String regex = searchWord.replace("*", ".*");
+        dp[0][0]=true;
 
-        regex = regex.replace("+", ".+");
-
-        return regex;
+        for(int j=1;j<=m;j++)
+        {
+            if(p.charAt(j-1)=='*')
+            {
+               dp[0][j] = dp[0][j-1];
+            }
+        }
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=m;j++)
+            {
+                if(s.charAt(i-1)==p.charAt(j-1))
+                {
+                    dp[i][j] = dp[i-1][j-1];
+                }
+                else if(p.charAt(j-1)=='*')
+                {
+                    dp[i][j] = dp[i-1][j] || dp[i][j-1];
+                }
+                else if(p.charAt(j-1)=='+')
+                {
+                    dp[i][j] = dp[i-1][j] || dp[i-1][j-1];
+                }
+                else {
+                    dp[i][j]=false;
+                }
+            }
+        }
+        return dp[n][m];
     }
 
 
